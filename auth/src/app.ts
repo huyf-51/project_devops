7b1cd07a7fa12d964a79fc2d5c7207da19f93a65
Huy <dgh2003lqm@gmail.com>
2024-11-24 13:29:25 +0700

    test ci for auth service

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -8,6 +8,21 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+import apm from 'elastic-apm-node';
+
+apm.start({
+    // Override service name from package.json
+    // Allowed characters: a-z, A-Z, 0-9, -, _, and space
+    serviceName: 'test',
+
+    // Use if APM Server uses API keys for authentication
+    apiKey: 'YUZyTFZwTUJxbG9XOFlrLS1RenI6ZHBWMVBHQzFRMTZKN2lHY2FrYlJZUQ==',
+
+    // Set custom APM Server URL (default: http://127.0.0.1:8200)
+    serverUrl: 'https://test-fd7764.apm.us-east-1.aws.elastic.cloud:443',
+
+    environment: 'staging',
+});
 
 const app = express();
 app.set('trust proxy', true);
@@ -26,6 +41,11 @@ app.use(signupRouter);
 
 app.all('*', async (req, res) => {
     throw new NotFoundError();
+});
+
+app.use((err: any, req: any, res: any, next: any) => {
+    apm.captureError(err);
+    next(err);
 });
 
 app.use(errorHandler);
